d9f5bf16fac17d84b71ef22a4937b4df0e670a09
chloeyeo <yeochloe01@gmail.com>
2024-08-01 11:17:55 +0900

    changed appjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          HIII this is Chloe trying to test github actions for frontend
+          deployment on AWS <br /> Static deployment on S3 and CloudFront <br />{" "}
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
